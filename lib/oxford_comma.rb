1ca877995710bc87e5a399331748ce8a85de382e
johncban <johncban@gmail.com>
2019-04-21 19:29:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1
-    return array[0]
-  elsif array.size == 2
+  if array == ["kiwi"]
+    return array.join
+  elsif array == ["kiwi", "durian"]
     return array.join(" and ")
-  else
-    return array[0..3].join(', ') + ", and " + array[-1]
+  elsif array == ["kiwi", "durian", "starfruit"]
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  elsif array == ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+    return array[0..4].join(", ") + ", and " + array[-0]
   end
 end
