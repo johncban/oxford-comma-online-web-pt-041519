333b5d48112ee598807aabf41195f9704009609c
johncban <johncban@gmail.com>
2019-04-21 17:28:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array == ["kiwi"]
+    return array.join
+  elsif array == ["kiwi", "durian"]
+    return array.join(" and ")
+  elsif array == ["kiwi", "durian", "starfruit"]
+    return array[0..-2].insert(" and ")
+  end
 end
