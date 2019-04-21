3d63c987028aebfc139ba02c23a5b579e16c3504
johncban <johncban@gmail.com>
2019-04-21 19:57:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
     array[-2] << " and "
     array.join
   elsif array.length > 2
+    # Source: https://apidock.com/ruby/String/prepend
     array[-1].prepend "and "
     array.join(", ")
   end
