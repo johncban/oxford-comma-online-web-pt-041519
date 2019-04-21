9b5ac671d01beca00b1c88cef58b1141d18ebeea
johncban <johncban@gmail.com>
2019-04-21 19:20:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else
-    return array[0..3].join(', ') + ", and " + array[-1]
+    return array[0..4].join(', ') + ", and " + array[-1]
   end
 end
